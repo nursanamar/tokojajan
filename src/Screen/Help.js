08cc62cd3064517d0008eeb7b39c95456ca6d269
Nursan amar <nursan011@gmail.com>
2018-07-13 00:49:24 +0800

    help page

--- src/Screen/Help.js
+++ src/Screen/Help.js
@@ -1,21 +1,12 @@
 import React, { Component } from 'react';
-// import {  } from 'react-native';
-import { Container, Header, Card, Left, Right, Body, Title, Content, Text } from "native-base";
+import { WebView } from 'react-native';
+import { Spinner,Container, Header, Card, Left, Right, Body, Title, Content, Text } from "native-base";
 
 class Help extends Component {
 
     render() {
         return (
-            <Container>
-                <Header style={{ backgroundColor: "rgb(6, 92, 81)" }} androidStatusBarColor="rgb(4, 77, 66)">
-                    <Body>
-                        <Title>Help</Title>
-                    </Body>
-                </Header>
-                <Content>
-                    <Text>Help</Text>
-                </Content>
-            </Container>
+            <WebView renderLoading={() => (<Spinner />)} startInLoadingState source={{ uri: "http://tokojajan.com/app/help" }} />            
         );
     }
 
